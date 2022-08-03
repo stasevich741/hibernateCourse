d484941b6655faaac275ce1cefcbae91d8913238
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-03 18:01:50 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernatecourse/HibernateCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernatecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCourseApplication.class, args);
+	}
+
+}
